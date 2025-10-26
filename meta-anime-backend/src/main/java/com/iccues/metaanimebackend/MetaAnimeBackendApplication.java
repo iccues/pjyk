4c1d53a73295ab72ed67559824cc29b232d1f156
iccues <jimiccues@gmail.com>
2025-10-26 21:49:43 +0800

    创建后端项目。

---
+++ meta-anime-backend/src/main/java/com/iccues/metaanimebackend/MetaAnimeBackendApplication.java
@@ -0,0 +1,13 @@
+package com.iccues.metaanimebackend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MetaAnimeBackendApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(MetaAnimeBackendApplication.class, args);
+    }
+
+}
---
+++ meta-anime-backend/src/test/java/com/iccues/metaanimebackend/MetaAnimeBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package com.iccues.metaanimebackend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MetaAnimeBackendApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
